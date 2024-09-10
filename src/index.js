11d18bcdd95b3af4e157f3c031e37bb95ca0903e
prajwalgirhepunje <prajwalgirhepunje12@gmail.com>
2024-09-10 16:09:12 +0530

    index.js file added

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <div>
+    <h1 style={{
+        textAlign:"center",
+        color:"black",
+    }}>ALL GRETTING</h1>
+ <greting name="prajwal"  />
+ <GoodMoring name="samir" />
+ <GoodEvening  name="sumya"/>
+
+ </div>
+)
